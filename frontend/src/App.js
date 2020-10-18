21ca0346c428bf18147d293440a64e42e7a349d2
dogurasu <dugs134@gmail.com>
2020-10-17 23:35:16 -0700

    added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <h1>Welcome to Ecommerce Shop</h1>
+    <Header />
+    {/* py-3 gives u spacing at the top */}
+    <main className="py-3">
+      <Container>
+        <h1>Welcome to Ecommerce Shop</h1>
+      </Container>
+    </main>
+    <Footer />
     </>
   );
 }

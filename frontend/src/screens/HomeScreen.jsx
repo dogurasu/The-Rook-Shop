7493c1cc02f0ecfa0a3db51378951ddf74ddcf55
dogurasu <dugs134@gmail.com>
2020-10-19 16:47:27 -0700

    refactored code to use Redux instead of standard React state

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,40 +1,66 @@
-import React, { useState, useEffect } from 'react'; // add products as component level state, products is eventually going to be global state when we get into Redux
+import React, { useEffect } from 'react'; // add products as component level state, products is eventually going to be global state when we get into Redux
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { listProducts } from '../actions/productActions'
+// import axios from 'axios'; // get rid of bc now we have Redux
 // import products from '../products'; // now we request products from our backend
 
 const HomeScreen = () => {
-    const [products, setProducts ] = useState([]);
+    // we don't need to set products for our local, component level state anymore
+    // const [products, setProducts ] = useState([]);
+
+    // we're using hooks here
+    // in the past, we would've used a higher order method called Connect and we'd have to map state to props
+    const dispatch = useDispatch(); 
+
+    // i want to get the 'productList' piece of state that we provided as an argument to the combineReducers function in our store
+    // useSelector takes in an arrow function w/ 'state' as arg and returns which part of state we want
+    const productList = useSelector(state => state.productList);
+    const { loading, error, products} = productList; // we want all these parts of state that could be passed down
 
     useEffect(() => {
-        const getProducts = async () => {
-            try {
-                const { data } = await axios.get('/api/v1/products', {
-                    port: 5000
-                })
+        // code when we didn't use Redux
+        // const getProducts = async () => {
+        //     try {
+        //         const { data } = await axios.get('/api/v1/products', {
+        //             port: 5000
+        //         })
 
-                setProducts(data);
-            } catch(err) {
-                console.log(err);
-            }
-        }
+        //         setProducts(data);
+        //     } catch(err) {
+        //         console.log(err);
+        //     }
+        // }
+        // getProducts();
 
-        getProducts();
-    }, [])
+        // use the dispatch in useEffect hook because it does the same thing, makes request to backend to get products
+        dispatch(listProducts());
 
+    }, [dispatch])
+
+    // check to see if if it's loading so we can output a spinner
+    // we can also check to see if there's an error
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map(product => {
-                    return (
-                        <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-                            <Product product={product}/>
-                        </Col>
-                    )
-                })}
-            </Row>
+            {loading ? (
+            <Loader />
+            ) : error ? (
+            <Message variant='danger'>{error}</Message>
+            ) : (
+                <Row>
+                    {products.map(product => {
+                        return (
+                            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                                <Product product={product}/>
+                            </Col>
+                        )
+                    })}
+                </Row>
+            )}
         </>
     );
 }
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,28 +3,42 @@ import React, {useState, useEffect} from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProductDetails } from '../actions/productActions';
+
 // import products from '../products';
-import Axios from 'axios';
+import axios from 'axios';
 
 const ProductScreen = ({ match }) => {
     // grab the product where the _id == _id in the request parameters
     // const product = products.find(p => p._id === match.params.id)
 
-    const [product, setProduct] = useState({});
+    // pre redux
+    // const [product, setProduct] = useState({});
+
+    const dispatch = useDispatch();
+
+    const productDetails = useSelector(state => state.productDetails);
+    const { loading, error, product } = productDetails;
 
     useEffect(() => {
-        const getProduct = async () => {
-            try {
-                const { data } = await Axios.get(`/api/v1/products/${match.params.id}`);
-                setProduct(data);
-            } catch(err) {
-                console.log(err);
-            }
-        }
+        // pre redux
+        // const getProduct = async () => {
+        //     try {
+        //         const { data } = await axios.get(`/api/v1/products/${match.params.id}`);
+        //         setProduct(data);
+        //     } catch(err) {
+        //         console.log(err);
+        //     }
+        // }
+        // getProduct();
+        
+        dispatch(listProductDetails(match.params.id))
 
-        getProduct();
-    }, [match])
-
+    }, [dispatch, match])
+    
     return (
         <>
             <LinkContainer to="/">
@@ -34,75 +48,77 @@ const ProductScreen = ({ match }) => {
             <Link className="btn btn-light my-3" to="/">
                  Go Back
             </Link> */}
-            <Row>
-                <Col md={6}>
-                    {/* Image component from bootstrap */}
-                    <Image
-                        src={product.image}
-                        alt={product.name}
-                        fluid // fluid prevents img from exiting its container
-                    >
-                    </Image>
-                </Col>
-                <Col md={3}>
-                    {/* variant='flush' takes away the spacing/border */}
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <h2>{product.name}</h2>
-                        </ListGroup.Item>
-                    </ListGroup>
-                    <ListGroup.Item>
-                        <Rating
-                            value={product.rating}
-                            text={`${product.numReviews} reviews`}
-                        />
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        Price: ${product.price}
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        Description: {product.description}
-                    </ListGroup.Item>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <ListGroup variant="flush">
+            {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+                <Row>
+                    <Col md={6}>
+                        {/* Image component from bootstrap */}
+                        <Image
+                            src={product.image}
+                            alt={product.name}
+                            fluid // fluid prevents img from exiting its container
+                        >
+                        </Image>
+                    </Col>
+                    <Col md={3}>
+                        {/* variant='flush' takes away the spacing/border */}
+                        <ListGroup variant='flush'>
                             <ListGroup.Item>
-                                <Row>
-                                    {/* this is the label */}
-                                    <Col>
-                                        Price:
-                                    </Col>
-                                    <Col>
-                                        <strong>${product.price}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    {/* this is the label */}
-                                    <Col>
-                                        Status:
-                                    </Col>
-                                    <Col>
-                                        <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                {/* class="btn-block" lets the element stretch all the way across */}
-                                <Button
-                                    className="btn-block"
-                                    type="button"
-                                    disabled={product.countInStock  === 0}
-                                >
-                                    Add to Cart
-                                </Button>
+                                <h2>{product.name}</h2>
                             </ListGroup.Item>
                         </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
+                        <ListGroup.Item>
+                            <Rating
+                                value={product.rating}
+                                text={`${product.numReviews} reviews`}
+                            />
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Price: ${product.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: {product.description}
+                        </ListGroup.Item>
+                    </Col>
+                    <Col md={3}>
+                        <Card>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>
+                                    <Row>
+                                        {/* this is the label */}
+                                        <Col>
+                                            Price:
+                                        </Col>
+                                        <Col>
+                                            <strong>${product.price}</strong>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Row>
+                                        {/* this is the label */}
+                                        <Col>
+                                            Status:
+                                        </Col>
+                                        <Col>
+                                            <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    {/* class="btn-block" lets the element stretch all the way across */}
+                                    <Button
+                                        className="btn-block"
+                                        type="button"
+                                        disabled={product.countInStock  === 0}
+                                    >
+                                        Add to Cart
+                                    </Button>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                    </Col>
+                </Row>
+            )}
         </>
     )
 }

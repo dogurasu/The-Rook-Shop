ba179e489258ec5e852f5fbd39440b61e55e272b
dogurasu <dugs134@gmail.com>
2020-10-20 22:01:50 -0700

    added "Update Profile" endpoint to backend API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,13 +3,18 @@ const router = express.Router();
 import {
     authUser,
     getUserProfile,
-    registerUser
+    registerUser,
+    updateUserProfile
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser); // not protected, public
 router.post('/login', authUser);
-// we want to protect this route so we just add our middleware as our first arg
-router.route('/profile').get(protect, getUserProfile);
+// we want to protect these route so we just add our middleware as our first arg
+// depending on the request we receive, we'll route req to .get or .put
+router
+    .route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
 
 export default router;

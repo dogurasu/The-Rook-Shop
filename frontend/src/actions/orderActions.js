5925c1b53f46039787829a0a5b0ff5845cb9fdbc
dogurasu <dugs134@gmail.com>
2020-10-22 13:42:21 -0700

    created orderDetails Constants, Reducers, Actions and added it into the store.js

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,11 +1,14 @@
 import {
     ORDER_CREATE_REQUEST, 
     ORDER_CREATE_SUCCESS, 
-    ORDER_CREATE_FAILURE
+    ORDER_CREATE_FAILURE,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_FAILURE
 } from '../constants/orderConstants.js';
 import axios from 'axios';
 
-// updateUserProfile action -- takes in user, need to send a token so we getState
+// createOrder 
 export const createOrder = (order) => async (dispatch, getState) => {
     try {
         dispatch({
@@ -33,6 +36,39 @@ export const createOrder = (order) => async (dispatch, getState) => {
     } catch(err) {
         dispatch({
             type: ORDER_CREATE_FAILURE,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+}
+
+// updateUserProfile action -- takes in user, need to send a token so we getState
+export const getOrderDetails = (order_id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: ORDER_DETAILS_REQUEST
+        })
+
+        // getState.userLogin.userInfo
+        const { userLogin: { userInfo }} = getState();
+        const config = {
+            headers: {
+                // 'Content-Type': 'application/json', // don't need content type for GET request
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        // post request to 'api/v1/orders'
+        const { data } = await axios.get(`/api/v1/orders${order_id}`, config);
+
+        // we want to dispatch our user details
+        dispatch({
+            type: ORDER_DETAILS_SUCCESS,
+            payload: data,
+        })
+
+    } catch(err) {
+        dispatch({
+            type: ORDER_DETAILS_FAILURE,
             payload: err.response && err.response.data.message ? err.response.data.message : err.message
         })
     }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,18 +2,57 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
-
+import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers';
+import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers';
 // initialize reducer - {} if we don't have any yet
 // important bc productList is what shows as that piece of state
 
 // it's good to break everything into separate reducers because that makes it easier to debug and it causes less issues w/ the wrong piece of state being displayed
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart: cartReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer
 });
 
+// we're going to check if items exist in the cart (fetch it from localstorage)
+// if they exist, we'll parse the string back to JSON and store it ina  var
+// else, store an empty list
+
+// we'll also check if there is a shipping address that has been input
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : [];
+
+const userInfoFromStorage = localStorage.getItem('userInfo') 
+    ? JSON.parse(localStorage.getItem('userInfo')) 
+    : null;
+
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') 
+    ? JSON.parse(localStorage.getItem('shippingAddress')) 
+    : {};
+
+// if (localStorage.getItem('cartItems')) {
+//     const cartItemsFromStorage = JSON.parse(localStorage.getItem('cartItems'));
+// } else {
+//     const cartItemsFromStorage = []
+// }
+
 // if we want something to be loaded when the Redux store loads initially, we can put it in here
-const initialState = {};
+// when store loads, we load our cart (or what's not in it)
+const initialState = {
+    cart: { 
+        cartItems: cartItemsFromStorage, 
+        shippingAddress: shippingAddressFromStorage
+    },
+    userLogin: { userInfo: userInfoFromStorage }
+};
 
 // you can have an array of middleware and init it with thunk
 const middleware = [thunk]

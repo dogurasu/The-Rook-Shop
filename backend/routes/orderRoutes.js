b3abc5119fef9fc6ee040e20d22a169eeb293f8f
dogurasu <dugs134@gmail.com>
2020-10-22 15:31:39 -0700

    added endpoint for updating 'paid' status

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express';
 const router = express.Router();
 import {
-    addOrderItems
+    addOrderItems,
+    getOrderById,
+    updateOrderToPaid
 } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
-// post request to /api/v1/orders - then we should be able to call that function out 
+// post request to /api/v1/orders - then we should be able to call that function out
 router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;

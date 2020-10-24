6d9599dca608acfb0615f6d9d2a011761fc8ffd6
dogurasu <dugs134@gmail.com>
2020-10-24 12:59:37 -0700

    setup create and update Product endpoints

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,24 @@
 import express from 'express';
-import { getProducts, getProductById, deleteProduct } from '../controllers/productController.js';
+import { 
+    getProducts, 
+    getProductById, 
+    deleteProduct, 
+    updateProduct,
+    createProduct
+} from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 // initialize an instance of Express's Router
 const router = express.Router();
 
 // default route will be "api/v1/products/""
 // since every single one of our routes will be asynchronous, we can either utilize 'try, catch' blocks or 'express-async-handler
-router.route('/').get(getProducts);
+router.route('/')
+    .get(getProducts)
+    .post(protect, admin, createProduct)
+
 router.route('/:id')
     .get(getProductById)
-    .delete(protect, admin, deleteProduct);
+    .delete(protect, admin, deleteProduct)
+    .put(protect, admin, updateProduct);
 
 export default router;
